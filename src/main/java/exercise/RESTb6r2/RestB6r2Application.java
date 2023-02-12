0a3921d25b8c2345c840b9a3818f989780746bee
diegobc <cassasdiego@hotmail.com>
2023-02-12 18:41:34 +0100

    rubrica terminada

--- src/main/java/exercise/RESTb6r2/RestB6r2Application.java
+++ src/main/java/exercise/RESTb6r2/RestB6r2Application.java
@@ -2,10 +2,20 @@ package exercise.RESTb6r2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class RestB6r2Application {
 
+
+	 @RequestMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+	    public String home() {
+	        return "Nothing here. Go to <a href='/ejemplo'>/ejemplo</a>";
+	    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestB6r2Application.class, args);
 	}
