7da9c41f9e9e0d18360032bed0dc215efb77215e
diegobc <cassasdiego@hotmail.com>
2023-02-12 18:01:21 +0100

    first commit

---
+++ src/main/java/exercise/RESTb6r2/RestB6r2Application.java
@@ -0,0 +1,13 @@
+package exercise.RESTb6r2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestB6r2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestB6r2Application.class, args);
+	}
+
+}
---
+++ src/test/java/exercise/RESTb6r2/RestB6r2ApplicationTests.java
@@ -0,0 +1,13 @@
+package exercise.RESTb6r2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestB6r2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
